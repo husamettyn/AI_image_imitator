ca5e0353b62fdc8f13aa51d03f4f85d241a8db67
Husamettin <husamettyn@gmail.com>
2024-03-21 11:38:02 +0300

    path is done and some draw fixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import random
 
 def create_circle_matrix(image_path):
     # Get the size of the image
@@ -10,28 +11,21 @@ def create_circle_matrix(image_path):
     grayscale_image = image.convert('L')
     # Convert the grayscale image to a numpy array
     image_array = np.array(grayscale_image)
-    # Normalize the pixel values to the range [0, 1]
-    normalized_image = image_array / 255.0
     
     # Create a matrix of zeros with the same size as the image
-    matrix = np.zeros((height, width))
+    matrix = np.ones((height, width))
 
-    return matrix, normalized_image
+    return matrix*255, image_array
 
 def get_angle_pixel_coordinates(matrix, angle):
-    # Get the dimensions of the matrix
     height, width = matrix.shape
-    
     # Calculate the center coordinates of the circle
     center_x = width // 2
     center_y = height // 2
-    
-    # Calculate the radius of the circle
     radius = min(center_x, center_y)
     
     # Calculate the angle in radians
     angle_rad = np.deg2rad(angle)
-    
     # Calculate the x and y coordinates of the pixel for the given angle
     x = int(center_x + radius * np.cos(angle_rad))
     y = int(center_y + radius * np.sin(angle_rad))
@@ -42,11 +36,17 @@ def draw_circle(matrix):
     for i in range(0, 360):
         x, y = get_angle_pixel_coordinates(matrix, i)
         if 0 <= y < matrix.shape[0] and 0 <= x < matrix.shape[1]:
-            matrix[y, x] = 1
+            matrix[y, x] = 0
     
     return matrix
 
 def draw_line(matrix, start_x, start_y, end_x, end_y):
+    # DONE başta siyah üzerine beyaz çiziyormuşum onları düzenledim
+    # artık her turda şu aşağıdaki sayı kadar eksiltme yapıyor
+    # spesifik bir anlamı yok, karekök yapmayınca çok küçülüyor sayı o yüzden kök var.
+    
+    global path_count
+    decrease = (255 // np.sqrt(path_count)) * 3
     # Calculate the difference between the start and end coordinates
     delta_x = end_x - start_x
     delta_y = end_y - start_y
@@ -63,7 +63,7 @@ def draw_line(matrix, start_x, start_y, end_x, end_y):
     y = start_y
     for _ in range(steps):
         if 0 <= y < matrix.shape[0] and 0 <= x < matrix.shape[1]:
-            matrix[int(y), int(x)] = 1
+            matrix[int(y), int(x)] -= decrease
         x += step_x
         y += step_y
     
@@ -77,17 +77,51 @@ def draw_path(matrix, path):
     
     return matrix
 
-if __name__ == "__main__":
-    image_path = 'images/1.png'
-    matrix, normalized_image = create_circle_matrix(image_path)
-    path = [0, 45, 90, 135, 180, 225, 270, 315, 0]
-    angle2 = 45
-
-    draw_path(matrix, path)
-    draw_circle(matrix)
+def calc_fit(matrix, normalized_image):
+    # TODO: iki resim arasındaki farkı düzgünce değerlendirmemiz gerek
+    """
+    iki resmi karşılaştırırken ne yapmamız gerekiyor?
+    -> her path'in resimde oluşturduğu karşılığı resimdeki değerler ile karşılaştırabiliriz.
+    -> resim oluştuktan sonra genel duruma bakarak bir uygunluk hesaplayabiliriz.
+    bunu da mesela boyalı / boyasız oranına bakarak yapabiliriz.
+    -> 
+    """
+    # Calculate the absolute difference between the matrix and normalized image
+    diff = np.abs(matrix - normalized_image)
     
-    # Convert the matrix back to an image
-    circle_image = Image.fromarray((matrix * 255).astype(np.uint8))
+    # Calculate the fitness as the mean of the absolute differences
+    fitness = np.mean(diff)
+    
+    return fitness
 
-    # Display the image
-    circle_image.show()
+def initpath(path_number):
+    # DONE: Create a list to store the angles of the path
+    path = []
+    angle = random.randint(0, 360)
+    path.append(angle)
+    
+    for i in range(1, path_number):
+        previous_angle = path[i-1]
+        angle = random.randint(0, 360)
+        while angle == previous_angle:
+            angle = random.randint(0, 360)
+        path.append(angle)
+
+    return path
+
+# DONE yeni bir görsel ekledim, 1080*1080 çözünürlüğünde
+# elde edilen çizgiler güzel görünüyor. Bu path'i genetik algoritmaya uydurmamız lazım.
+# şu an sadece rastgele seçilmiş bir path'i çiziyoruz.
+
+image_path = 'images/3.png'
+matrix, image = create_circle_matrix(image_path)
+
+path_count = 500
+path = initpath(path_count)
+
+draw_path(matrix, path)
+draw_circle(matrix)
+
+# Convert the matrix back to an image
+circle_image = Image.fromarray((matrix / np.max(matrix) * 255).astype(np.uint8))
+circle_image.show()
